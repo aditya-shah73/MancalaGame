1976e7fd14a5a48ef85f11c4b598621fa8a71af7
aditya7395 <aditya7395@gmail.com>
2015-04-30 17:34:09 -0700

    Deleted Store and Chandges made to Pit

--- src/Pit.java
+++ src/Pit.java
@@ -1,29 +1,38 @@
 import java.util.ArrayList;
 
-
 public class Pit
 {
 	private ArrayList<Marble> storage;
+	public boolean isPit;
 	int count;
 	
-	public Pit(){
+	public Pit()
+	{
 		storage = new ArrayList<Marble>();
 		count = 0;
 	}
 	
-	public void addMarble(Marble m){
+	public void addMarble(Marble m)
+	{
 		storage.add(m);
 		count++;
 	}
 	
-	public void removeAll(){
+	public void removeAll()
+	{
 		storage = new ArrayList<Marble>();
 	}
 	
-	public void removeMarble(){
+	public void removeMarble()
+	{
 		if(storage.size() > 0)
+		{
 			storage.remove(storage.size()-1);
+		}
 	}
 	
-	
+	public boolean isAPit()
+	{
+		return isPit;
+	}
 }
