6fadcfc72e4ca3ee9317a7d1f6ad8b0a551b5532
Turtle101 <sohamshah100@gmail.com>
2015-05-03 15:16:16 -0700

    Added new class with main method.

---
+++ src/MancalaTester.java
@@ -0,0 +1,14 @@
+
+public class MancalaTester {
+	public static void main(String[] args) {
+
+		 
+		Board1 wood = new Board1();
+		//Board2 marble = new Board2();
+		
+		
+		wood.makeBoard();
+		//marble.makeBoard();
+	    }
+
+}
